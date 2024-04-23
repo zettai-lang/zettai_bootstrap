6444e1d352938e259333042d22770e940c6425e2
Matthew Toohey <contact@mtoohey.com>
2024-04-23 14:37:51 -0400

    fix flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,56 +13,52 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, ... }@inputs: {
-    overlays.default = final: _: {
-      zettai_bootstrap = final.ocamlPackages.buildDunePackage rec {
-        pname = "zettai_bootstrap";
-        version = "0.1.0";
+  outputs = { self, nixpkgs, utils, starpath }: {
+    overlays = rec {
+      expects-starpath = final: _: {
+        zettai_bootstrap = final.callPackage
+          ({ ocamlPackages }:
+            ocamlPackages.buildDunePackage {
+              pname = "zettai_bootstrap";
+              version = "0.1.0";
 
-        minimalOcamlVersion = "4.14.1";
-        duneVersion = "3";
+              minimalOcamlVersion = "4.14.1";
+              duneVersion = "3";
 
-        checkInputs = buildInputs ++ [ final.ocamlPackages.ppx_inline_test ];
-        buildInputs = with final.ocamlPackages; [
-          ounit2
-          ppx_deriving
-          ppx_variants_conv
-          starpath
-        ];
+              doCheck = true;
+              buildInputs = with ocamlPackages; [
+                ounit2
+                ppx_assert
+                ppx_deriving
+                ppx_expect
+                ppx_variants_conv
+                ocamlPackages.starpath
+              ];
 
-        src = builtins.path { path = ./..; name = "zettai_bootstrap-src"; };
+              src = builtins.path { path = ./..; name = "zettai_bootstrap-src"; };
+            })
+          { };
       };
+      default = nixpkgs.lib.composeManyExtensions [
+        starpath.overlays.default
+        expects-starpath
+      ];
     };
   } // utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
-        overlays = [ inputs.starpath.overlays.default self.overlays.default ];
+        overlays = [ self.overlays.default ];
         inherit system;
       };
-      inherit (pkgs) zettai_bootstrap mkShell ocamlformat ocamlPackages;
-      inherit (ocamlPackages) bisect_ppx dune_3 findlib ocaml ocaml-lsp ounit2
-        ppx_assert ppx_deriving ppx_expect ppx_inline_test ppx_variants_conv
-        starpath;
+      inherit (pkgs) mkShell ocamlformat ocamlPackages zettai_bootstrap;
+      inherit (ocamlPackages) bisect_ppx ocaml-lsp;
     in
     {
       packages.default = zettai_bootstrap;
 
       devShells.default = mkShell {
-        packages = [
-          bisect_ppx
-          dune_3
-          findlib
-          ocaml
-          ocamlformat
-          ocaml-lsp
-          ounit2
-          ppx_assert
-          ppx_deriving
-          ppx_expect
-          ppx_inline_test
-          ppx_variants_conv
-          starpath
-        ];
+        inputsFrom = [ zettai_bootstrap ];
+        packages = [ bisect_ppx ocamlformat ocaml-lsp ];
       };
     });
 }
